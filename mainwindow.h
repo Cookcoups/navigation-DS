ab152a539e3fe3a6046838cf314d216f1c275086
niuruiyu <1801844018@qq.com>
2025-04-22 10:42:29 +0800

    补充了mainwindow的几个辅助函数声明，完成了视图的初步初始化
    视图可以显示黑白，但是还没染色，没有实现刷新边和刷新点的功能，注意：视图的大小为600x600！mythread文件还没有补充，界面还没有插入槽函数

--- mainwindow.h
+++ mainwindow.h
@@ -1,6 +1,6 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
+#include <QGraphicsScene>
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
@@ -16,6 +16,15 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    //最近100个点
+    void setnearpoint(double x,double y);
+    //最短路
+    void setshortestpath(int x,int y,int time);
+    //刷新路径
+    void updatepath();
+    //刷新点
+    void updatepoint();
+    QGraphicsScene *m_scene;
 
 private:
     Ui::MainWindow *ui;
